c4d968eaf0ff32de8d7979ab05aea2fb2d43b17d
Ale Breuer <ale.breuer14@gmail.com>
2022-07-26 13:41:39 -0300

    Arreglos para Reentrega

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 //Desafio incorporar Eventos
-//Cambiar los promp por input, que tenga las mismas funciones pero que haya eventos.
 
 
 class Club {
-    constructor( id, nombre, creacion, liga, titulos, goleador) {
+    constructor(id, nombre, creacion, liga, titulos, goleador) {
         this.id = id
-        this.nombre = nombre
-        this.creacion = creacion
-        this.liga = liga
+        this.nombre = nombre.toUpperCase()
+        this.creacion = parseInt(creacion)
+        this.liga = liga.toUpperCase()
         this.titulos = titulos
-        this.goleador = goleador
+        this.goleador = goleador.toUpperCase()
     }
 }
 
 
-let  id = 0, nombre, creacion, liga, titulos, goleador, continuar, respuesta
+let id = 0, nombre, creacion, liga, titulos, goleador, continuar, respuesta
 
 const clubes = []
 
 const form = document.getElementById("idForm")
-
 const divClubes = document.getElementById("divClubes")
 const botonClubes = document.getElementById("botonClubes")
 
 
 
-form.addEventListener("submit",(event) => {
-    event.preventDefault() 
-
-    nombre = document.getElementById("idNombreClub").value 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    nombre = document.getElementById("idNombreClub").value
     creacion = parseInt(document.getElementById("idCreacion").value)
     liga = document.getElementById("idLiga").value
     titulos = parseInt(document.getElementById("idTitulos").value)
     goleador = document.getElementById("idGoleador").value
-    
-
-    const club = new Club(id,nombre, creacion, liga, titulos, goleador)
-
+    const club = new Club(id, nombre, creacion, liga, titulos, goleador)
     clubes.push(club)
-
     form.reset()
 })
 
@@ -57,57 +50,69 @@ botonClubes.addEventListener("click", () => {
     })
 })
 
+//Buscar por club
 
+const divRespuesta1 = document.getElementById("respuesta1")
+const botonBuscarClub = document.getElementById("botonBuscarClub")
 
-
-const divRespuesta1 = document.getElementById("respuesta1") 
-
-function buscarClub(clubes) {
-    let clubBuscado = clubes.find(clubes => clubes.nombre == inputBuscarClub.value)
-
-    if(clubBuscado == undefined) {
-        divRespuesta1.innerHTML += `
-        <div>
-        <p> Club Inexistente en la base de Datos </p>
-        </div>`
-    } else {
+function buscarClub(busqueda) {
+    let clubBuscado = clubes.find(club => club.nombre == busqueda)
+    divRespuesta1.innerHTML = ""
+    if (clubBuscado) {
         divRespuesta1.innerHTML += `
         <div>
         <p> El Club Buscado ${clubBuscado.nombre} </p>
         </div>`
+    } else {
+        divRespuesta1.innerHTML += `
+        <div>
+        <p> Club Inexistente en la base de Datos </p>
+        </div>`
     }
 }
 
+botonBuscarClub.addEventListener("click", () => {
+    const inputBuscarClub = document.getElementById("idBuscarClub").value.toUpperCase()
+    buscarClub(inputBuscarClub)
+})
 
-function buscarAntiguedad(clubes) {
-    let clubesBuscados = clubes.filter(clubes => clubes.creacion >= inputAñoClub.value) 
+//Buscar por antiguedad, si buscas por año, por lo general te debería devolver clubes creados en ese año.
+
+const botonAnioClub = document.getElementById("botonAñoClub")
+
+function buscarAntiguedad(busqueda) {
+    let clubesBuscados = clubes.filter(club => club.creacion === busqueda)
     console.log(clubesBuscados)
-
-    if(clubesBuscados.length == 0) {
+    divRespuesta1.innerHTML = ""
+    if (clubesBuscados.length == 0) {
         divRespuesta1.innerHTML += `
         <div>
         <p>No existen clubes con dichas caracteristicas   </p>
         </div>
         `
     } else {
-        const divRespuesta1 = document.getElementById("respuesta1")
-        for(let x = 0; x < clubesBuscados.length; x++) {
+        for (let x = 0; x < clubesBuscados.length; x++) {
             divRespuesta1.innerHTML += `
             <div>
-            <p> ${clubesBuscados[x].nombre} se creo despues de ${inputAñoClub.value}  </p>
+            <p> ${clubesBuscados[x].nombre} se creó en ${busqueda}  </p>
             </div>`
         }
 
     }
 }
 
+botonAnioClub.addEventListener("click", () => {
+    //Con getElementById estás consultando la etiqueta, no su valor
+    //const inputAñoClub = parseInt(document.getElementById("añoClub")) Si hago asi no anda la funcion(pense que era asi)
+    const inputAnioClub = parseInt(document.getElementById("añoClub").value)
+    buscarAntiguedad(inputAnioClub)
+})
 
 
-function buscarGoleador(clubes) {
-
-    let clubBuscado = clubes.find(clubes => clubes.goleador == inputMaxGoleador.value)
-
-    if(clubBuscado == undefined) {
+function buscarGoleador(busqueda) {
+    let clubBuscado = clubes.find(clubes => clubes.goleador == busqueda)
+    divRespuesta1.innerHTML = ""
+    if (clubBuscado == undefined) {
         divRespuesta1.innerHTML += `
         <div>
         <p>Goleador inexistente en la base de datos</p>
@@ -123,40 +128,23 @@ function buscarGoleador(clubes) {
 
 function sumarTitulos(clubes) {
     let acumulador = 0
-    let titulos = clubes.map(clubes => clubes.titulos) 
-    titulos.forEach(titulo => acumulador += titulo ) 
+    let titulos = clubes.map(clubes => clubes.titulos)
+    divRespuesta1.innerHTML = ""
+    titulos.forEach(titulo => acumulador += titulo)
 
     divRespuesta1.innerHTML += `
     <div>
     <p> Titulos totales: ${acumulador} </p>
     </div>`
-    
 }
 
-
-//Primera Funcion
-const inputBuscarClub = document.getElementById("idBuscarClub")
-const botonBuscarClub = document.getElementById("botonBuscarClub")
-
-botonBuscarClub.addEventListener("click", () => {
-    buscarClub(clubes)
-})
-
-//Segunda Funcion
-const inputAñoClub = document.getElementById("añoClub") 
-//const inputAñoClub = parseInt(document.getElementById("añoClub")) Si hago asi no anda la funcion(pense que era asi)
-const botonAñoClub = document.getElementById("botonAñoClub")
-
-botonAñoClub.addEventListener("click",() => {
-    buscarAntiguedad(clubes)
-})
-
 //Tercer Funcion:
-const inputMaxGoleador = document.getElementById("inputMaxGoleador")
+
 const botonMaxGoleador = document.getElementById("botonMaxGoleador")
 
 botonMaxGoleador.addEventListener("click", () => {
-    buscarGoleador(clubes)
+    const inputMaxGoleador = document.getElementById("inputMaxGoleador").value.toUpperCase()
+    buscarGoleador(inputMaxGoleador)
 })
 
 //Cuarta Funcion:
@@ -165,16 +153,6 @@ const botonSumar = document.getElementById("botonSumar")
 botonSumar.addEventListener("click", () => {
     sumarTitulos(clubes)
 })
-
-
-
-
-
-
-
-
-
-
 
 
 
